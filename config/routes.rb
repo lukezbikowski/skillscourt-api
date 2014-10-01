b2964a0ee79d1cb82bf4979c9a278405ac3c9ab5
duketon <michael.kendra@iinet.net.au>
2014-10-02 08:36:21 +1000

    Create :api :v1 route namespacing, constrained to the api subdomain, include resource routes for :students

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   
-  namespace :api do
-    constraints subdomain: 'api' do
-      match '/v1/students', {:via => :get, :to => 'students#index'}
+  namespace :api, path: '', constraints: {subdomain: 'api'} do
+    namespace :v1 do
+      resources :students
     end
   end
 end
