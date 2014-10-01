2df8cf9c9de3cdeb5e3dc2656688237c01212535
duketon <michael.kendra@iinet.net.au>
2014-10-02 08:44:20 +1000

    Include default format of JSON for the api routes.
    The server will respond with JSON to a GET request coming to /students. You don't have to specify /students.json. However if the request goes to /students.html then rails with throw a 'UnknownFormat' excpetion and respond with a '406 Not Acceptable'. There's probably a way to handle this explicitly, however I'm not sure how to do this. I'm assuming here that we have no use for this data in html format.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   
-  namespace :api, path: '', constraints: {subdomain: 'api'} do
+  namespace :api, path: '', constraints: {subdomain: 'api'}, defaults: {format: 'json'} do
     namespace :v1 do
       resources :students
     end
