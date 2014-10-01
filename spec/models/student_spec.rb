8e2ba094cf56eb5c2ddcebd6d1f9831bc9abd85e
Michael Kendra <michael.kendra@iinet.net.au>
2014-10-01 03:44:05 +0000

    Add validation to not allow duplicate student names

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -12,5 +12,12 @@ RSpec.describe Student, :type => :model do
     it "should create a new instance of the student profile" do
       expect(Student.create!(@attributes)).to be_valid
     end
+
+    it "should fail to create a student with a duplicate name" do
+       Student.create @attributes
+       student = Student.create @attributes
+
+       expect(student).to be_invalid
+    end
   end
 end
