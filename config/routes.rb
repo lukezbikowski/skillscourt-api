9d86a5cff1b01d7cbdc27c409353afac902b95c0
Michael Kendra <michael.kendra@iinet.net.au>
2014-10-01 14:16:16 +0000

    Add namespace for api, add route for students index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
+  
+  namespace :api do
+    constraints subdomain: 'api' do
+      match '/v1/students', {:via => :get, :to => 'students#index'}
+    end
+  end
 end
