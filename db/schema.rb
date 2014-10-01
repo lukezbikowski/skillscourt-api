fd6d2bcaf0eda645b3d426c23a26a09913c7de31
Michael Kendra <michael.kendra@iinet.net.au>
2014-10-01 03:32:05 +0000

    Add schema for students table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929173016) do
+ActiveRecord::Schema.define(version: 20141001032711) do
+
+  create_table "students", force: true do |t|
+    t.string "name"
+    t.string "email"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
