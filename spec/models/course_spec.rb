4975f748bd2398c71d864634d4b8f6f37e25ab19
Peter Supina <peter.supina@gmail.com>
2014-10-18 21:35:29 +0000

    Refactor model spec for Course using shoulda-matchers

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,31 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Course, :type => :model do
-  before(:each) do
-    @attributes = {
-      name: "Ruby for Beginners",
-      description: "You will get to know just the basics.",
-      starting_at: "2014-11-01 17:00:00",
-      rating: 4
+  let(:attributes) {
+      {
+        name: "Ruby for Beginners",
+        description: "You will get to know just the basics.",
+        starting_at: "2014-11-01 17:00:00",
+        rating: 4
+      }
     }
-  end
 
-  describe "validation" do
-    it "should create a new instance of the student profile" do
-      expect(Course.create!(@attributes)).to be_valid
-    end
+  subject(:test_course) { Course.create!(attributes) }
 
-    it "should fail to create a course when name is not given" do
-       course = Course.create
-
-       expect(course).to be_invalid
-    end
-
-    it "should fail to create a course with a duplicate name" do
-      Course.create @attributes
-      course = Course.create @attributes
-
-      expect(course).to be_invalid
-    end
-  end
+  it { is_expected.to be_valid }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
 end
