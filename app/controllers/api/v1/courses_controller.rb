85ee8e56f48a76bca781dec5906ce3b40ccec1ba
Peter Supina <peter.supina@gmail.com>
2014-10-20 21:04:40 +0000

    Add implementation of the index action in Courses controller

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::CoursesController < Api::V1::ApiController
   respond_to :json
 
+  def index
+    respond_with courses
+  end
+
   def show
     respond_with course
   end
 
   private
+
+  def courses
+    @courses ||= Course.all
+  end
 
   def course
     @course ||= Course.find params[:id]
